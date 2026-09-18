3911db3b3a13e849fc0c4f22745586f0cd8c5662
Gabriel <gabrieltzmb@gmail.com>
2026-09-18 08:44:13 -0300

    feat: Banco de dados do projeto

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,3 +1,12 @@
+import { useState } from "react"
+
+//Array de objetos contendo o estado inicial do cardápio
+const cardapio=[
+  { id: 1, nome: "Combo-01", preco: 25.00, disponivel: true, quantidade: 0},
+  { id: 2, nome: "Combo-02", preco: 35.00, disponivel: true, quantidade: 0 },
+  { id: 3, nome: "Combo-03", preco: 45.00, disponivel: false, quantidade: 0 },
+  { id: 4, nome: "Combo-04", preco: 55.00, disponivel: true, quantidade: 0 },
+];
 
 const Pedido = () => {
   return (
